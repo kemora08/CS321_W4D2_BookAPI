dc376afd2db17af4ee416b8f71db4c254e7c5021
kemora08 <kevin_mora89@hotmail.com>
2020-06-24 11:12:17 -0500

    Initial Commit

---
+++ CS321_W4D2_BookAPI.Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CS321_W4D2_BookAPI.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CS321_W4D2_BookAPI.Infrastructure/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CS321_W4D2_BookAPI.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
